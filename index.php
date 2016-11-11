e700a35b84d88828d98dec1318b7177442ec785d
Moritz Ewert <moritz.ewert@wunderfactory.de>
2016-11-11 14:26:17 +0100

    foo

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Schneepfötchen</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -40,7 +40,7 @@
                   {
                       ?>
                       <div class="alert alert-success">
-                          <strong>Holy slug Batman!</strong> Der Code war richtig!
+                          <strong>Glückwunsch!</strong> Die Kombination für das Schloss lautet <code>1747</code>.
                       </p>
                       <?php
                   }
